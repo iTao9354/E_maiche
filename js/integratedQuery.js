d27fdb21395e6ac0e199bd3abbc892775d886d76
CynthiaJin <1404700953@qq.com>
2017-09-16 23:41:05 +0800

    integratedQuery and productDetail

--- js/integratedQuery.js
+++ js/integratedQuery.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2017-09-11 23:08:50
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-09-15 23:18:24
+* @Last Modified time: 2017-09-16 23:23:17
 */
 
 $(function() {
@@ -17,27 +17,45 @@ var conditionObj = {};
 function chooseConditions() {	
 	$('.condition-list > a').click(function() {
 		var _this = $(this),
-			selectedHtml = _this.html(),
+			selectedHtml = _this.text(),
 			selectedName = _this.attr('name');
 		_this.addClass('selected').siblings().removeClass('selected');
 		$('.hasChoosed > .iconfont').show();
 		conditionObj[selectedName] = selectedHtml;
 		$('.hasChoosed > p').empty()
 		for(var name in conditionObj) {
-			$('.hasChoosed > p').append('<span>'+conditionObj[name]+'<i class="iconfont" onclick="remCondition(this,\''+name+'\');">&#xe622;</i></span>');
+			$('.hasChoosed > p').append('<span><span>'+conditionObj[name]+'</span><i class="iconfont" onclick="remCondition(this,\''+name+'\');">&#xe622;</i></span>');
 		}		
-		console.log(conditionObj)
 	})
 		
 }
 function remCondition(ele, name) {
-	$(ele).parent().remove();
+	var removedSelectedEle = $('.condition-list > .selected[name="'+name+'"]');
+	$(ele).parent().remove();	
+	if(removedSelectedEle.text() +'--'+$(ele).prev().text()) {
+		removedSelectedEle.removeClass('selected').parent().children().eq(0).addClass('selected');
+	}
 	delete conditionObj[name];
+	if($.isEmptyObject(conditionObj)) {
+		$('.hasChoosed > .iconfont').hide();
+	}
+}
+
+// 一键清空
+function remAllConditions(ele) {
+	$('.hasChoosed > p').empty();
+	conditionObj = {};
+	$(ele).hide();
+	$('.condition-list > a').removeClass('selected');
+	$('.condition-list').each(function() {
+		$(this).children().eq(0).addClass('selected');
+	})
 }
 
 // 初始化表格
 var productTableData = [
-    { 
+    {
+    	productId: "adhiheeiw22", 
     	productImg: "product-thumb-1", 
     	productDesc: null,
     	productTitle: "1111奥迪A6L2014款 30 FSI", 
@@ -50,6 +68,7 @@ var productTableData = [
     	issuedDate: "2017-07-27"
     },
     { 
+    	productId: "desfefcds3",
     	productImg: "product-thumb-2", 
     	productDesc: null,
     	productTitle: "2222奥迪A6L2014款 30 FSI", 
@@ -62,6 +81,7 @@ var productTableData = [
     	issuedDate: "2017-07-27"
     },
     { 
+    	productId: "t4frfdds3",
     	productImg: "product-thumb-1", 
     	productDesc: null,
     	productTitle: "1111奥迪A6L2014款 30 FSI", 
@@ -74,6 +94,7 @@ var productTableData = [
     	issuedDate: "2017-07-27"
     },
     { 
+    	productId: "fef3f3",
     	productImg: "product-thumb-2", 
     	productDesc: null,
     	productTitle: "2222奥迪A6L2014款 30 FSI", 
@@ -86,6 +107,7 @@ var productTableData = [
     	issuedDate: "2017-07-27"
     },
     { 
+    	productId: "defdsfw3dds3",
     	productImg: "product-thumb-1", 
     	productDesc: null,
     	productTitle: "1111奥迪A6L2014款 30 FSI", 
@@ -98,6 +120,7 @@ var productTableData = [
     	issuedDate: "2017-07-27"
     },
     { 
+    	productId: "dddddr3fds3",
     	productImg: "product-thumb-2", 
     	productDesc: null,
     	productTitle: "2222奥迪A6L2014款 30 FSI", 
@@ -142,7 +165,7 @@ function initProductTable(){
 				"orderable": false,
 				"render" : function(data, type,row) {
 					  var html = '';
-					  html += '<a href="javascript:;"><img src="img/'+data+'.jpg"></a>';
+					  html += '<a href="productDetail.html#'+row.productId+'"><img src="img/'+data+'.jpg"></a>';
 				      return html;
 				   }
 			},
@@ -152,7 +175,7 @@ function initProductTable(){
 				"orderable": false,
 				"render" : function(data, type,row) {
 					  var html = '';
-					  html += '<a href="javascript:;">'+row.productTitle+'</a>';
+					  html += '<a href="productDetail.html#'+row.productId+'">'+row.productTitle+'</a>';
 					  html += '<p>';
 					  html += '排量：<span>'+row.displacement+'</span>';
 					  html += '上牌日期：<span>'+row.registeredDate+'</span>';
@@ -192,9 +215,4 @@ function initProductTable(){
 		],
 		"serverSide": false      // 数据从后台调取时此处为true，在公用的common-js.jsp已定义
 	});
-}
-
-//刷新数据  true  整个刷新      false 保留当前页刷新
-function reloadTableData(isCurrentPage){
-	$("#userTable").dataTable().fnDraw(isCurrentPage==null?true:isCurrentPage);
 }
---
+++ js/productDetail.js
@@ -0,0 +1,20 @@
+/*
+* @Author: Administrator
+* @Date:   2017-09-11 23:08:50
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-09-16 23:39:45
+*/
+
+$(function() {
+	var url = window.location.href,
+		productId = url.substring(url.indexOf('#')+1);
+	// $.ajax({
+	// 	url: '',
+	// 	data: {
+	// 		productId: productId
+	// 	},
+	// 	success: function(data) {
+
+	// 	}
+	// })
+})
